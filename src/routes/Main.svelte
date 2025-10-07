15e7c9dc7f441d0a8f33a698880683e9e6a9e6b3
Masahjor <98082444+Masahjor@users.noreply.github.com>
2025-10-07 09:35:32 +0200

    Allo

--- src/routes/Main.svelte
+++ src/routes/Main.svelte
@@ -1,12 +1,15 @@
 <!-- Script -->
 <script lang="ts">
-const messages = ["Oh no!", "It is the dive rocket launcher!", "Take action regarding that individual.", "Relax, I'll handle it.", "I am the angry pumpkin.", "*BOOM*", "The architect and the builder.", "They arrive calmly from their escalator with a sense of purpose.", "They are playing at their residence!", "The head is soaring!"];
+  // const messages = ["Oh no!", "It is the dive rocket launcher!", "Take action regarding that individual.", "Relax, I'll handle it.", "I am the angry pumpkin.", "*BOOM*", "The architect and the builder.", "They arrive calmly from their escalator with a sense of purpose.", "They are playing at their residence!", "The head is soaring!"];
 
-messages.forEach((msg, index) => {
-  setTimeout(() => {
-    console.log(msg);
-  }, index * 2000);
-});
+  // messages.forEach((msg, index) => {
+  //   setTimeout(() => {
+  //     console.log(msg);
+  //   }, index * 2000);
+  // });
+  
+  console.log("Allo");
+
 </script>
 
 
